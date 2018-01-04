1aafc39e9a07c81a70b95276dfb2f81c4bab6294
liuweibing <weibing.liu@joy2u.cn>
2018-01-04 18:23:54 +0800

    the third day of learning python

--- hello.py
+++ hello.py
@@ -522,7 +522,7 @@ while True:
 	except StopIteration:
 		break
 '''
-
+'''
 #高阶函数
 def add(x,y,f):
 	return f(x)+f(y)
@@ -530,6 +530,353 @@ print add(-5,6,abs)
 def f(x):
 	return x*x
 r = map(f,[1,2,3,4,5,6,7])
+print type(r)
 print r
 r1 = [x*x for x in [1,2,3,4,5,6,7]]
+print type(r1)
 print r1
+digits = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+from functools import reduce
+def fn(x,y):
+	return x*10+y
+print reduce(fn,[1,2,3,4,5])
+def char2num(s):
+	return digits[s]
+print reduce(fn,map(char2num,'13579'))	#结合reduce和map将一个字符串转变成整数
+#当然python有这个功能函数int轻松可以做到
+print int('123456') #我们只不过是自己写了个转换成整数的函数
+def int2(s):
+	def fn(x,y):
+		return x*10+y
+	def char2num(s):
+		return digits[s]
+	return reduce(fn,map(char2num,s))
+S = "12345678"
+A = int2(S)
+print type(A)
+print A
+#用lambda可以简化一点
+def char2num(s):
+	return digits[s]
+def int3(s):
+	return reduce(lambda x,y:x*10+y,map(char2num,s))
+B = int3(S)
+print type(B)
+print B
+'''
+'''
+print 'ABC'.lower()
+#利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字
+def normalize(name):
+	S = name[0].upper()
+	H = name[1:].lower()
+	return S+H
+List = ['adam', 'LISA', 'barT']
+print map(normalize,List)
+#请编写一个prod()函数，可以接受一个list并利用reduce()求积
+from functools import reduce
+def prod(L):
+	def redu(x,y):
+		return x*y
+	return reduce(redu,L)
+print '3*5*7*9 =',prod([3,5,7,9])
+#利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+digits = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,}
+#方法一：这是自己捣鼓出来的
+def str2flost(s):
+	def fn(x,y):
+		return x*10+y
+	def fn3(l):
+		n = 1
+		sum = 0
+		for i in l:
+			x = i*10**-n
+			n = n+1
+			sum = sum+x
+		return sum
+	def str2num(s):
+		return digits[s]
+	index = s.find(".")
+	zhengshus = s[0:index]
+	xiaoshus = s[index+1:]
+	zhengshu = reduce(fn,map(str2num,zhengshus))
+	xiaoshu = fn3(map(str2num,xiaoshus))
+	return zhengshu+xiaoshu
+A=str2flost("123.456")
+print type(A)
+print A
+#方法二：看别人搞得
+def str2floast(s):
+	def str2num(i):
+		return digits[i]
+	def fn(x,y):
+		return x*10+y
+	l = s.split(".")
+	n = len(l[1])
+	zhengshu = reduce(fn,map(str2num,l[0]))
+	xiaoshu = reduce(fn,map(str2num,l[1]))
+	return zhengshu+xiaoshu*10**-n
+B = str2floast("1234.56789")
+print type(B)
+print B
+'''
+'''
+#filter()函数用于过滤序列
+def is_odd(n):
+	return n % 2 == 1
+print map(is_odd,[1,2,3,4,5,6,])		#这两个例子即可看出他们之间的区别，map是将返回值加入新列表
+print filter(is_odd,[1,2,3,4,5,6])		#filter是将返回值为真的加入新列表
+A=" I am   boy !   "
+print len(A)
+print A.strip()   #它只是删除字符头尾的空白
+print len(A.strip(" "))
+def notempty(s):
+	return s and s.strip()
+print filter(notempty,"I am boy ! ")    #这种方法就可以把所有的空白都删掉
+print filter(notempty,["","I am",None,"boy"," ","!",])
+'''
+'''
+#用filter求素数
+def CreatNum():
+	n = 1
+	while True:
+		n = n + 2
+		yield n
+def ZhiShu(n):
+	return lambda x: x % n > 0
+def primes():
+	yield 2
+	it = CreatNum()
+	while True:
+		n = next(it)
+		yield n
+		it = filter(ZhiShu(n),it)
+for n in primes():
+	if n < 1000:
+		print n
+	else:
+		break
+#方法二
+import math
+def getPrime(maxNum):
+    primeList = []
+    for x in range(2,maxNum+1):
+        x_sqrt = math.sqrt(x)
+        for prime in primeList:
+            if prime > x_sqrt:
+                primeList.append(x)
+                break
+            if x%prime==0:
+                break
+        else:
+            primeList.append(x)
+    return primeList
+print getPrime(1000)
+#方法3
+def prime(x):  
+    for i in range(2,x):  
+        if x%i==0:  
+            return False  
+        if i==x-1:  
+            return True  
+output=filter(prime,range(2,10)) 
+print output
+'''
+'''
+#回数是指从左向右读和从右向左读都是一样的数，例如12321，909。请利用filter()筛选出回数：
+def is_palindrome(n):
+	s = str(n)
+	if s == s[::-1]:
+		return True
+	else:
+		return False
+print filter(is_palindrome,range(1000))
+#字符串反转的多种方法：
+A = "Hello World"
+#方法一：
+def fz1(s):
+	return s[::-1]
+print fz1(A)
+#方法二:
+def fz2(s):
+	l = list(s)
+	l.reverse()
+	
+	str = ''
+	for i in l:
+		str = str+i
+	return str
+#这4行可以用下面的方法代替
+	return ''.join(l)
+print fz2(A)
+#方法3
+from functools import reduce
+def fz3(s):
+	l = list(s)
+	return reduce(lambda x,y:y+x ,l)
+print fz3(A)
+
+'''	
+'''	
+#排序算法
+l = [1,-9,23,-46,29]
+print sorted(l)
+print sorted(l,key=abs)  #sorted()函数也是一个高阶函数，它还可以接收一个key函数来实现自定义的排序，例如按绝对值大小排序
+l1 = ['bob', 'about', 'Zoo', 'Credit']
+print sorted(l1,key=str.lower)  #忽略大小写排序
+print sorted(l1,key=str.lower,reverse=True) #反转排序后的结果
+l2 = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+def by_name(list):
+	return sorted(list,key=lambda x:x[0])  #key指定的函数将作用于list的每一个元素上，并根据key函数返回的结果进行排序
+print by_name(l2)
+def by_score(list):
+	return sorted(list,key=lambda x:x[1])
+print by_score(l2)
+#匿名函数lambda
+f = lambda x,y,z:x+y+x   #lambda只是一个表达式，函数体比def简单很多。
+print f(1,2,3)
+def action(x):			
+	return lambda y:x+y	 #这个函数返回了一个lambda的表达式
+a = action(22)			 #这个a现在就成了一个函数等价于a = lambda y:22+y
+print a(3)				 #这个就是把3作为y，然后通过a函数计算出结果25
+#这里定义了一个action函数，返回了一个lambda表达式。
+#其中lambda表达式获取到了上层def作用域的变量名x的值。
+#a是action函数的返回值，a(22)，即是调用了action返回的lambda表达式。
+'''	
+'''	
+#函数作为返回值，闭包
+def lazy_sum(*args):
+	def sum():
+		s = 0
+		for i in args:
+			s = s + i
+		return s
+	return sum
+l = [1,2,3,4,5]
+print lazy_sum(*l)#这是一个求和函数，我们需要结果时需要再执行
+print lazy_sum(*l)()
+#在这个例子中，我们在函数lazy_sum中又定义了函数sum，并且，内部函数sum可以引用外部函数lazy_sum的参数和局部变量，当lazy_sum返回函数sum时，相关参数和变量都保存在返回的函数中，这种称为“闭包（Closure）”的程序结构拥有极大的威力。
+#请再注意一点，当我们调用lazy_sum()时，每次调用都会返回一个新的函数，即使传入相同的参数：
+f1 = lazy_sum(*l)
+f2 = lazy_sum(*l)  #这两个函数是不同的函数
+print f1 != f2     #从结果可知这两个不是等价的但是他们的值是相同的
+print f1() == f2()
+def count():
+	fs = []
+	for i in range(1,4):
+		fs.append(i*i)
+	return fs
+print count()   #这种是直接返回一个列表，里面是我们需要的结果
+
+def count1():
+	fs = []
+	for i in range(1,4):
+		def fun():
+			return i*i
+		fs.append(fun)
+	return fs
+print count1()	#这种是返回一个列表里面都是函数，需要再次执行才能得出我们的结果
+print count1()[0](),count1()[1](),count1()[2]() #看结果知道都是9，这是因为在返回函数的时候i的值都循环成了3
+
+def count2():
+	fs = []	
+	def fun1(j):
+		def fun2():
+			return j*j
+		return fun2
+	for i in range(1,4):
+		fs.append(fun1(i))
+	return fs
+print count2()[0](),count2()[1](),count2()[2]() #看结果可以知道这是我们需要的结果，这是因为我们加了一个函数，fun1把i的值固定了，所以才能得出想要的结果
+
+
+#利用闭包返回一个计数器函数，每次调用它返回递增整数：
+def CreateCouter():
+	def CreateNum():
+		n = 1
+		yield n
+		while True:
+			n = n+1
+			yield n
+	it = CreateNum()
+	def counter():		
+		return next(it)
+	return counter
+counterA = CreateCouter()
+counterB = CreateCouter()
+print counterA(),counterA(),counterA(),counterA(),counterA()
+print counterB(),counterB(),counterB(),counterB()
+'''		
+'''
+print filter(lambda x:x%2==1,range(1,20))
+f = lambda x:x%2==1
+print filter(f,range(1,20))
+'''
+'''
+#装饰器
+#现在，假设我们要增强now()函数的功能，比如，在函数调用前后自动打印日志，但又不希望修改now()函数的定义，这种在代码运行期间动态增加功能的方式，称之为“装饰器”（Decorator）。
+def log(func):
+	def wrapper(*args,**kw):
+		print 'call %s()' %func.__name__
+		return func(*args,**kw)
+	return wrapper
+@log
+def now():
+	print "2018-01-04"
+f = now
+f()
+print f.__name__  #可以看到结果是wrapper，而不是原先的now,这就可能导致一些依赖函数签名的代码执行错误，所以我们就可以用functools.wraps来做到保存原有的函数名：所以一个完整的decorator写法如下：
+import functools
+def log(func):
+	@functools.wraps(func)
+	def wrapper(*args,**kw):
+		print 'call %s():' %func.__name__
+		return func(*args,**kw)
+	return wrapper
+#如果装饰器也要带参数，写法如下:
+def log(text):
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args,**kw):
+			print '%s %s()' %(text,func.__name__)
+			return func(*args,**kw)
+		return wrapper
+	return decorator
+@log("execute")
+def fun1(name,age):
+	print 'my name is %s,i\'m %s' %(name,age)
+fun1("liuweibing",28)
+'''
+#请设计一个decorator，它可作用于任何函数上，并打印该函数的执行时间：
+import functools,time
+def metric(func):
+	@functools.wraps(func)
+	def wrapper(*args,**kw):
+		t = time.time()
+		t1 = time.strftime('%Y-%m-%d-%H:%M:%S',time.localtime(t))
+		print '%s start executed in %s' %(func.__name__,t1)
+		rs = func(*args,**kw)
+		print '%s end executed in %s,it execute %s ms' %(func.__name__,time.strftime('%Y-%m-%d-%H:%M:%S',time.localtime()),time.time()-t)
+		return rs
+	return wrapper
+@metric
+def fast(x, y):
+    time.sleep(1)
+    return x + y;
+
+@metric
+def slow(x, y, z):
+    time.sleep(1)
+    return x * y * z;
+
+f = fast(11, 22)
+s = slow(11, 22, 33)
+if f != 33:
+    print u'测试失败!'
+elif s != 7986: 
+    print u'测试失败!'
+else:
+	print u'测试成功'
+
+
+
